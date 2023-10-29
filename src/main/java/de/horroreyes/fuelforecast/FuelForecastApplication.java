6c9be15c219c5ba1b495af9d9b20532cea6fa324
Christian Clausen <chriss.clausen@googlemail.com>
2023-10-29 22:43:48 +0100

    Initial Commit

---
+++ src/main/java/de/horroreyes/fuelforecast/FuelForecastApplication.java
@@ -0,0 +1,13 @@
+package de.horroreyes.fuelforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelForecastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuelForecastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/horroreyes/fuelforecast/FuelForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package de.horroreyes.fuelforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelForecastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
